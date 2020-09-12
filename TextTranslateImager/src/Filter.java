6414a954b9f2d82e49675a7f493b018abb36262c
Moi <Moi@DESKTOP-3PIL9HA>
2020-09-12 13:34:30 +0200

    Premier jet du smoother + adaptation du main pour le test

--- TextTranslateImager/src/Filter.java
+++ TextTranslateImager/src/Filter.java
@@ -1,3 +1,4 @@
+import java.awt.image.BufferedImage;
 
 public class Filter {
 	
@@ -24,6 +25,63 @@ public class Filter {
 		
 	}
 	
-	public static int[][] test(){ return null;}
+	public static void smoother(BufferedImage bufImage, int weight,int radius, int padding){
+		System.out.println("ALGO SMOOTHER : ");
+		int[] packageTemp = new int[radius*radius];
+		int cursor_x = 0;
+		int cursor_y = 0;
+		int maxCursor_x = bufImage.getWidth()-radius;
+		int maxCursor_y = bufImage.getHeight()-radius;
+		int debordement_x;
+		int debordement_y;
+		int moyenneDirectTest=0;
 
+		if(weight>100) weight=100;
+		else if(weight<0) weight=0;
+		
+		
+		for(cursor_y=0 ; cursor_y < bufImage.getHeight() ; cursor_y+=padding) {
+			for(cursor_x=0 ; cursor_x < bufImage.getWidth() ; cursor_x+=padding) {
+				
+				if(cursor_x+radius >= bufImage.getWidth()) debordement_x= cursor_x+radius - bufImage.getWidth();
+				else debordement_x = 0;
+				if(cursor_y+radius >= bufImage.getHeight()) debordement_y= cursor_y+radius - bufImage.getHeight();
+				else debordement_y = 0;
+				
+
+				for(int i=0; i<radius*radius ; i++) {
+					packageTemp[i]=0;
+				}
+				
+				bufImage.getRGB(cursor_x, cursor_y, radius-debordement_x, radius-debordement_y, packageTemp, 0, radius);
+				
+				for(int i=0; i<radius*radius ; i++) {
+					moyenneDirectTest += packageTemp[i];
+				}
+				moyenneDirectTest /= radius*radius;
+				
+				//Faire la transition de chaque couleur vers la moyenne avec le "weight" en pourcentage
+				
+				for(int i=0; i<radius*radius ; i++) {
+						if(packageTemp[i] < moyenneDirectTest )
+							packageTemp[i] += (Math.abs(moyenneDirectTest-packageTemp[i]) / 100)*weight;
+						else
+							packageTemp[i] -= (Math.abs(moyenneDirectTest-packageTemp[i]) / 100)*weight;
+					
+				}
+				bufImage.setRGB(cursor_x, cursor_y, radius-debordement_x, radius-debordement_y, packageTemp,0,radius);
+				
+			}
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
--- TextTranslateImager/src/main.java
+++ TextTranslateImager/src/main.java
@@ -15,18 +15,25 @@ public class main {
 	
 
 	public static void main(String[] args) {
+		BufferedImage image;
+		List<String> listeMots;
+		String listeMotsConcatene;
+		List<Integer> listeRGB;
+		
 		
 		System.out.println("Debut du logiciel de traduction de texte en image");
 		System.out.println("Vérification du fichier texte");
 		
-		List<String> listeMots = Utils.lireTexteToString();
-		String listeMotsConcatene = Utils.remplacerCaractereSpeciaux(listeMots);
-		List<Integer> listeRGB = Utils.convertStringToRGB(listeMotsConcatene);
+		listeMots = Utils.lireTexteToString();
+		listeMotsConcatene = Utils.remplacerCaractereSpeciaux(listeMots);
+		listeRGB = Utils.convertStringToRGB(listeMotsConcatene);
 		int imageSize = (int) Math.ceil(Math.sqrt(listeRGB.size()));
 		int[][] matriceRGB = Utils.listToArray(listeRGB , imageSize);
-		matriceRGB = Filter.repeupler(matriceRGB,1500);
+		//matriceRGB = Filter.repeupler(matriceRGB,1500);
+		
 		imageSize = (int) Math.ceil(Math.sqrt(Utils.nbrElement(matriceRGB)));
-		BufferedImage image = Utils.createImage(imageSize , imageSize , matriceRGB);
+		image = Utils.createImage(imageSize , imageSize , matriceRGB);
+		Filter.smoother(image, 90, 30 , 13);
 		Utils.saveImage(image);
 		
 		System.out.println("Fini");
